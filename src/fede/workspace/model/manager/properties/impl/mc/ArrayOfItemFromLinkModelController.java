37b20e66663c130caa63b3f0985f962c9144e7bb
chomats <chomats@localhost>
2009-11-25 13:47:01 +0100

    move file and delete package

--- src/fede/workspace/model/manager/properties/impl/mc/ArrayOfItemFromLinkModelController.java
+++ src/fede/workspace/model/manager/properties/impl/mc/ArrayOfItemFromLinkModelController.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package fede.workspace.model.manager.properties.impl;
+package fede.workspace.model.manager.properties.impl.mc;
 
 import java.util.Collection;
 
