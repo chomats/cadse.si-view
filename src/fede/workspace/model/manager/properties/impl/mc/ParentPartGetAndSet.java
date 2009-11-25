d4035bb0ffed5d57cb51e89b4b5d6e533dadb779
chomats <chomats@localhost>
2009-11-25 13:47:01 +0100

    move file and delete package

--- src/fede/workspace/model/manager/properties/impl/mc/ParentPartGetAndSet.java
+++ src/fede/workspace/model/manager/properties/impl/mc/ParentPartGetAndSet.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package fede.workspace.model.manager.properties.impl;
+package fede.workspace.model.manager.properties.impl.mc;
 
 import fr.imag.adele.cadse.core.CadseException;
 import fr.imag.adele.cadse.core.Item;
